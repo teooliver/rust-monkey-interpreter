9ffe98652a640fed9d64d6db027bb8e261bd9296
Teo Oliver <teo.liz.oli@gmail.com>
2022-09-29 12:11:29 +0100

    feat: wip

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -15,54 +15,7 @@
 // Why do we need the "Literal" value in golang then?
 
 // ===================================================================
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Token {
-    Illegal,
-    Blank,
-    Eof,
-
-    // Identifiers + literals
-    Ident(String),
-    Int(i64),
-    String(String),
-    Bool(bool),
-
-    // Statements
-    Assign,
-    If,
-    Else,
-
-    // Operators
-    Plus,
-    Minus,
-    Bang,
-    Asterisk,
-    Slash,
-
-    Equal,
-    NotEqual,
-    LessThan,
-    LessThanEqual,
-    GreaterThan,
-    GreaterThanEqual,
-
-    // Delimiters
-    Comma,
-    Colon,
-    Semicolon,
-    Lparen,
-    Rparen,
-    Lbrace,
-    Rbrace,
-    Lbracket,
-    Rbracket,
-
-    // Reseved keywords
-    Func,
-    Let,
-    Return,
-}
+use crate::token::Token;
 
 #[derive(Default, Debug)]
 struct Lexer {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod lexer;
+pub mod token;
 
 fn main() {
     println!("Hello, world!");
