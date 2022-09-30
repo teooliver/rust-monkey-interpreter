dd409ba4beb58c0fae597104f9c853566036a484
Teo Oliver <teo.liz.oli@gmail.com>
2022-09-30 18:20:35 +0100

    chore: extra repl to its own folder

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
 pub mod lexer;
+mod repl;
 pub mod token;
 use std::io;
 
-use crate::lexer::Lexer;
-
-fn main() -> io::Result<()> {
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input)?;
-
-    println!("You typed: {}", input.trim());
-
-    let mut l = Lexer::new(&input);
-
-    loop {
-        let tok = l.next_token();
-        if tok == token::Token::Eof {
-            break;
-        }
-        println!("CURRENT TOKEN: {:?}", tok);
-    }
-
-    Ok(())
+fn main() {
+    repl::start();
 }
--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,1 +1,23 @@
-// fn start()
+use std::io;
+
+use crate::{lexer::Lexer, token};
+
+pub fn start() -> io::Result<()> {
+    println!("Hello! This is the Monkey programming language!");
+    println!("Feel free to type in commands");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
+    let mut l = Lexer::new(&input);
+
+    loop {
+        let tok = l.next_token();
+        if tok == token::Token::Eof {
+            break;
+        }
+        println!("{:?}", tok);
+    }
+
+    Ok(())
+}
